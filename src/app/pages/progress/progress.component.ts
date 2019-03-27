1b8f86b64f118fc8e3a699a649d2e4522f160048
Eduardo Estrada <ceed.educacion@gmail.com>
2019-03-27 01:29:02 -0300

    Input, output y viewchild

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,13 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
-  progreso1: Number = 50;
-  // progreso2: number = 30;
-
+  progreso1: number = 20;
+  progreso2: number = 30;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
